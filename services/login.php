86d0e821426ae2a278374760252e292fa343f148
jabie393 <wahyurojabi393@gmail.com>
2025-08-01 21:50:46 +0700

    minor fix

--- services/login.php
+++ services/login.php
@@ -2,33 +2,39 @@
 include '../config/config.php';
 session_start();
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-// Query untuk mendapatkan data pengguna berdasarkan email
-$query = "SELECT * FROM users WHERE email = ?";
-$stmt = $conn->prepare($query);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
+    // Query untuk mendapatkan data pengguna berdasarkan email
+    $query = "SELECT * FROM users WHERE email = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-if ($result->num_rows === 1) {
-    $user = $result->fetch_assoc();
-    // Verifikasi password
-    if (password_verify($password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['user_username'] = $user['username'];
-        $_SESSION['role'] = $user['role'];
-        header("Location: ../dashboard");
-        exit();
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        // Verifikasi password
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+            header("Location: ../dashboard");
+            exit();
+        } else {
+            $_SESSION['error_message'] = "Password salah. Silakan coba lagi.";
+        }
     } else {
-        $_SESSION['error_message'] = "Password salah. Silakan coba lagi.";
+        $_SESSION['error_message'] = "Email tidak ditemukan.";
     }
-} else {
-    $_SESSION['error_message'] = "Email tidak ditemukan.";
-}
 
-// Redirect kembali ke login jika ada kesalahan
-header("Location: ../login");
-exit();
+    // Redirect kembali ke login jika ada kesalahan
+    header("Location: ../login");
+    exit();
+} else {
+    $_SESSION['error_message'] = "Silakan isi email dan password.";
+    header("Location: ../login");
+    exit();
+}
 ?>
